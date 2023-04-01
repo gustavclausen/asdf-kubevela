1d94cb0893a2ad27aa4934c73e7c094db8655db6
Gustav Kofoed Clausen <gustav.clausen@zeronorth.com>
2023-04-02 00:06:38 +0200

    feat: first version

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for kubevela.
 GH_REPO="https://github.com/kubevela/kubevela"
 TOOL_NAME="kubevela"
 TOOL_TEST="vela version"
@@ -12,9 +11,48 @@ fail() {
   exit 1
 }
 
+get_arch() {
+  local arch=""
+
+  case "$(uname -m)" in
+  x86_64 | amd64) arch='amd64' ;;
+  aarch64 | arm64) arch="arm64" ;;
+  *)
+    fail "Arch '$(uname -m)' not supported!"
+    ;;
+  esac
+
+  echo -n $arch
+}
+
+get_platform() {
+  local platform=""
+
+  case "$(uname | tr '[:upper:]' '[:lower:]')" in
+  darwin) platform="darwin" ;;
+  linux) platform="linux" ;;
+  windows) platform="windows" ;;
+  *)
+    fail "Platform '$(uname -m)' not supported!"
+    ;;
+  esac
+
+  echo -n $platform
+}
+
+get_ext() {
+  local platform="$1"
+  ext="tar.gz"
+
+  if [ "$platform" == "windows" ]; then
+    ext="zip"
+  fi
+
+  echo -n $ext
+}
+
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if kubevela is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -26,26 +64,24 @@ sort_versions() {
 
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    grep -oE 'refs/tags/v[0-9]+.[0-9]+.[0-9]+(-.*)?' |
+    cut -d/ -f3- |
+    sed 's/^v//'
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if kubevela has other means of determining installable versions.
   list_github_tags
 }
 
 download_release() {
-  local version filename url
-  version="$1"
-  filename="$2"
+  local version="$1"
+  local release_file="$2"
+  local download_path="$3"
 
-  # TODO: Adapt the release URL convention for kubevela
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  url="$GH_REPO/releases/download/v${version}/$release_file"
 
   echo "* Downloading $TOOL_NAME release $version..."
-  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+  curl "${curl_opts[@]}" -o "$download_path" -C - "$url" || fail "Could not download $url"
 }
 
 install_version() {
@@ -61,7 +97,6 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Assert kubevela executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
